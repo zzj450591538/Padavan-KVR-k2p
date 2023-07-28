7b24c9e53002ae5241a2de2bfa2677c8003cc427
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2023-07-28 12:44:52 +0800

    Add files via upload

---
+++ trunk/user/scripts/wxsend.sh
@@ -0,0 +1,297 @@
+#!/bin/bash
+#copyright by hiboy
+
+#测试号申请地址：https://mp.weixin.qq.com/debug/cgi-bin/sandbox?t=sandbox/login
+#使用教程：https://opt.cn2qq.com/opt-file/测试号配置.pdf
+#官网新建模板内容必须为{{1.DATA}} {{2.DATA}}{{3.DATA}} {{4.DATA}}{{5.DATA}} {{6.DATA}} 
+
+#appid：
+wxsend_appid=wx91374521du92f9a6d
+
+#appsecret：
+wxsend_appsecret=b2a02552wf82xa2r0d65x8197560
+
+#微信号：
+wxsend_touser=ok5hz6FH118JI20shyFd7YCPqg
+
+#模板ID:
+wxsend_template_id=pHcIGVV55sfgOC-8mYG4sAB-mApPONBSA2fc2bj0
+
+#推送功能在下方102行，修改你开启推送的功能，默认全部开启的
+
+D="/etc/storage/cron/crontabs"
+F="$D/`nvram get http_username`"
+data="$(date "+%G-%m-%d_%H:%M:%S")"
+get_token () {
+touch /tmp/wx_access_token
+access_token="$(cat /tmp/wx_access_token)"
+http_type="$(curl -L -k "https://api.weixin.qq.com/cgi-bin/get_api_domain_ip?access_token=$access_token")"
+get_api_domain="$(echo $http_type | grep ip_list)"
+if [ ! -z "$get_api_domain" ] ; then
+echo "Access token 有效"
+else
+http_type="$(curl -L -k "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$wxsend_appid&secret=$wxsend_appsecret")"
+access_token="$(echo $http_type | grep -o "\"access_token\":\"[^\,\"\}]*" | awk -F 'access_token":"' '{print $2}')"
+if [ ! -z "$access_token" ] ; then
+expires_in="$(echo $http_type | grep -o "\"expires_in\":[^\,\"\}]*" | awk -F 'expires_in":' '{print $2}')"
+logger -t "【微信推送】" "获取 Access token 成功，凭证有效时间，单位： $expires_in 秒"
+echo -n "$access_token" > /tmp/wx_access_token
+else
+errcode="$(echo $http_type | grep -o "\"errcode\":[^\,\"\}]*" | awk -F ':' '{print $2}')"
+if [ ! -z "$errcode" ] ; then
+errmsg="$(echo $http_type | grep -o "\"errmsg\":\"[^\,\"\}]*" | awk -F 'errmsg":"' '{print $2}')"
+logger -t "【微信推送】" "获取 Access token 返回错误码: $errcode"
+logger -t "【微信推送】" "错误信息: $errmsg"
+access_token=""
+echo -n "" > /tmp/wx_access_token
+fi
+fi
+fi
+}
+
+send_message () {
+get_token
+access_token="$(cat /tmp/wx_access_token)"
+if [ ! -z "$access_token" ] ; then
+curl -k -H "Content-type: application/json;charset=UTF-8" -H "Accept: application/json" -H "Cache-Control: no-cache" -H "Pragma: no-cache" -X POST -d '{"touser":"'"$wxsend_touser"'","template_id":"'"$wxsend_template_id"'","data":{"1":{"value":"'"$1 $data "'"},"2":{"value":"'"$data  "'"},"3":{"value":"'"$1"'"},"4":{"value":"'"$2"'"},"5":{"value":"'"$3"'"}}}' "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=$access_token"
+logger -t "【微信推送】" "推送成功》》$1 $2 $3"
+else
+logger -t "【微信推送】" "推送失败，获取 Access token 错误，请看看哪里问题？"
+fi
+}
+
+wxsend_keep  () {
+[ ! -z "`pidof wxsendfile.sh`" ] && logger -t "【微信推送】" "启动成功"
+logger -t "【微信推送】" "守护进程启动"
+sed -Ei '/微信推送守护进程|^$/d' "$F"
+cat >> "$F" <<-OSC
+*/1 * * * * test -z "\`pidof wxsendfile.sh\`"  && /etc/storage/wxsend.sh start #微信推送守护进程
+OSC
+}
+
+wxsend_close () {
+  sed -Ei '/微信推送守护进程|^$/d' "$F"
+  killall wxsendfile.sh
+  killall -9 wxsendfile.sh
+  rm -rf /etc/storage/wxsendfile.sh
+  sleep 3
+[ -z "`pidof wxsendfile.sh`" ] && logger -t "【微信推送】" "进程已关闭"
+}
+
+wxsend_start () {
+ killall wxsendfile.sh
+ killall -9 wxsendfile.sh
+ rm -rf /etc/storage/wxsendfile.sh
+ if [ ! -f "/etc/storage/wxsendfile.sh" ] || [ ! -s "/etc/storage/wxsendfile.sh" ] ; then
+         cat > "/etc/storage/wxsendfile.sh" <<-\EEE
+#!/bin/bash
+
+#这里修改你要推送的功能 去掉1表示不启用 填=1 表示启用 自定义推送还需要往下自己添加命令 在253行
+
+#WAN口IP变动 启用填=1
+wxsend_notify_1=1
+
+#设备接入提醒 启用填=1
+wxsend_notify_2=1
+
+#设备上、下线提醒 启用填=1
+wxsend_notify_3=1
+
+#自定义推送提醒 启用填=1
+wxsend_notify_4=1
+
+mkdir -p /tmp/var
+resub=1
+# 获得外网地址v4
+    arIpAddress4() {
+    curltest=`which curl`
+    if [ -z "$curltest" ] || [ ! -s "`which curl`" ] ; then
+	wget -qO- http://ipecho.net/plain | xargs echo
+    else
+	curl -4 -k https://ifconfig.co/ip
+    fi
+    }
+# 读取最近外网地址v4
+    lastIPAddress4() {
+        [ ! -e "/etc/storage/wxsend_lastIPAddress4" ] && touch /etc/storage/wxsend_lastIPAddress4
+        inter4="/etc/storage/wxsend_lastIPAddress4"
+        cat $inter4
+    }
+ # 获得外网地址v6
+    arIpAddress6() {
+    curltest=`which curl`
+    if [ -z "$curltest" ] || [ ! -s "`which curl`" ] ; then
+        wget -T 5 -t 3 --user-agent "$user_agent" --quiet --output-document=- "https://ipv6.icanhazip.com"
+    else
+        curl -L -k --user-agent "$user_agent" -s "https://ipv6.icanhazip.com"
+    fi
+    }
+# 读取最近外网地址v6
+    lastIPAddress6() {
+        [ ! -e "/etc/storage/wxsend_lastIPAddress6" ] && touch /etc/storage/wxsend_lastIPAddress6
+        inter6="/etc/storage/wxsend_lastIPAddress6"
+        cat $inter6
+    }
+
+while [ "1" = "1" ];
+do
+
+curltest=`which curl`
+ping_text=`ping -4 223.5.5.5 -c 1 -w 2 -q`
+ping_time=`echo $ping_text | awk -F '/' '{print $4}'| awk -F '.' '{print $1}'`
+ping_loss=`echo $ping_text | awk -F ', ' '{print $3}' | awk '{print $1}'`
+if [ ! -z "$ping_time" ] ; then
+    echo "ping：$ping_time ms 丢包率：$ping_loss"
+ else
+    echo "ping：失效"
+fi
+if [ ! -z "$ping_time" ] ; then
+echo "online"
+if [ "$wxsend_notify_1" = "1" ] ; then
+    hostIP4=$(arIpAddress4)
+    hostIP6=$(arIpAddress6)
+    #hostIP=`echo $hostIP | head -n1 | cut -d' ' -f1`
+    if [ "$hostIP4"x = "x"  ] ; then
+        curltest=`which curl`
+        if [ -z "$curltest" ] || [ ! -s "`which curl`" ] ; then
+            [ "$hostIP4"x = "x"  ] && hostIP4=`wget -qO- http://ipecho.net/plain | xargs echo`
+        else
+            [ "$hostIP4"x = "x"  ] && hostIP4=`curl -4 -k https://ifconfig.co/ip`
+        fi
+    fi
+    if [ "$hostIP6"x = "x"  ] ; then
+        curltest=`which curl`
+        if [ -z "$curltest" ] || [ ! -s "`which curl`" ] ; then
+            [ "$hostIP6"x = "x"  ] && hostIP6=` wget -T 5 -t 3 --user-agent "$user_agent" --quiet --output-document=- "https://ipv6.icanhazip.com"`
+        else
+            [ "$hostIP6"x = "x"  ] && hostIP6=`curl -L -k --user-agent "$user_agent" -s "https://ipv6.icanhazip.com"`
+        fi
+    fi
+    lastIP4=$(lastIPAddress4)
+    lastIP6=$(lastIPAddress6)
+    if [ "$lastIP4" != "$hostIP4" ] && [ ! -z "$hostIP4" ] ; then
+    sleep 60
+        hostIP4=$(arIpAddress4)
+        #hostIP4=`echo $hostIP4 | head -n1 | cut -d' ' -f1`
+        lastIP4=$(lastIPAddress4)
+    fi
+    if [ "$lastIP4" != "$hostIP4" ] && [ ! -z "$hostIP4" ] ; then
+       /etc/storage/wxsend20.sh send_message "【WAN口IPV4变动】" "目前`nvram get computer_name` IP: ${hostIP4}" "上次 IP: ${lastIP4} " &
+        echo -n $hostIP4 > /etc/storage/wxsend_lastIPAddress4
+    fi
+    if [ "$lastIP6" != "$hostIP6" ] && [ ! -z "$hostIP6" ] ; then
+    sleep 60
+        hostIP6=$(arIpAddress6)
+        #hostIP6=`echo $hostIP6 | head -n1 | cut -d' ' -f1`
+        lastIP6=$(lastIPAddress6)
+    fi
+    if [ "$lastIP6" != "$hostIP6" ] && [ ! -z "$hostIP6" ] ; then
+       /etc/storage/wxsend20.sh send_message "【WAN口IPV6变动】" "目前`nvram get computer_name` IP: ${hostIP6}" "上次 IP: ${lastIP6}" &
+        echo -n $hostIP6 > /etc/storage/wxsend_lastIPAddress6
+    fi
+fi
+if [ "$wxsend_notify_2" = "1" ] ; then
+    # 获取接入设备名称
+    touch /tmp/var/wxsend_newhostname.txt
+    echo "接入设备名称" > /tmp/var/wxsend_newhostname.txt
+    #cat /tmp/syslog.log | grep 'Found new hostname' | awk '{print $7" "$8}' >> /tmp/var/wxsend_newhostname.txt
+    cat /tmp/static_ip.inf | grep -v "^$" | awk -F "," '{ if ( $6 == 0 ) print "【内网IP："$1"，ＭＡＣ：\n"$2"，名称："$3"】  "}' >> /tmp/var/wxsend_newhostname.txt
+    # 读取以往接入设备名称
+    touch /etc/storage/wxsend_hostname.txt
+    [ ! -s /etc/storage/wxsend_hostname.txt ] && echo "接入设备名称" > /etc/storage/wxsend_hostname.txt
+    # 获取新接入设备名称
+    awk 'NR==FNR{a[$0]++} NR>FNR&&a[$0]' /etc/storage/wxsend_hostname.txt /tmp/var/wxsend_newhostname.txt > /tmp/var/wxsend_newhostname相同行.txt
+    awk 'NR==FNR{a[$0]++} NR>FNR&&!a[$0]' /tmp/var/wxsend_newhostname相同行.txt /tmp/var/wxsend_newhostname.txt > /tmp/var/wxsend_newhostname不重复.txt
+    if [ -s "/tmp/var/wxsend_newhostname不重复.txt" ] ; then
+        content=`cat /tmp/var/wxsend_newhostname不重复.txt | grep -v "^$" | sed "s#[^^]\([0-9]\.\) #\n\1 #g" | awk 'NR==1 {print $1}')"`
+	content2=`cat /tmp/var/wxsend_newhostname不重复.txt | grep -v "^$" | sed "s#[^^]\([0-9]\.\) #\n\1 #g" | awk 'NR==2 {print $1}')"`
+        /etc/storage/wxsend20.sh send_message "【`nvram get computer_name`新设备加入】" "${content}" "${content2}" &
+         cat /tmp/var/wxsend_newhostname不重复.txt | grep -v "^$" >> /etc/storage/wxsend_hostname.txt
+    fi
+fi
+if [ "$wxsend_notify_3" = "1" ] ; then
+    # 设备上、下线提醒
+    # 获取接入设备名称
+    touch /tmp/var/wxsend_newhostname.txt
+    echo "接入设备名称" > /tmp/var/wxsend_newhostname.txt
+    #cat /tmp/syslog.log | grep 'Found new hostname' | awk '{print $7" "$8}' >> /tmp/var/wxsend_newhostname.txt
+    cat /tmp/static_ip.inf | grep -v "^$" | awk -F "," '{ if ( $6 == 0 ) print "【内网IP："$1"，ＭＡＣ：\n"$2"，名称："$3"】  "}' >> /tmp/var/wxsend_newhostname.txt
+    # 读取以往上线设备名称
+    touch /etc/storage/wxsend_hostname_上线.txt
+    [ ! -s /etc/storage/wxsend_hostname_上线.txt ] && echo "接入设备名称" > /etc/storage/wxsend_hostname_上线.txt
+    # 上线
+    awk 'NR==FNR{a[$0]++} NR>FNR&&a[$0]' /etc/storage/wxsend_hostname_上线.txt /tmp/var/wxsend_newhostname.txt > /tmp/var/wxsend_newhostname相同行_上线.txt
+    awk 'NR==FNR{a[$0]++} NR>FNR&&!a[$0]' /tmp/var/wxsend_newhostname相同行_上线.txt /tmp/var/wxsend_newhostname.txt > /tmp/var/wxsend_newhostname不重复_上线.txt
+    if [ -s "/tmp/var/wxsend_newhostname不重复_上线.txt" ] ; then
+        content=`cat /tmp/var/wxsend_newhostname不重复_上线.txt | grep -v "^$" | sed "s#[^^]\([0-9]\.\) #\n\1 #g" | awk 'NR==1 {print $1}')"`
+	content2=`cat /tmp/var/wxsend_newhostname不重复_上线.txt | grep -v "^$" | sed "s#[^^]\([0-9]\.\) #\n\1 #g" | awk 'NR==2 {print $1}')"`
+       /etc/storage/wxsend20.sh send_message "【`nvram get computer_name`设备上线提醒】" "${content}" "${content2}" &
+        cat /tmp/var/wxsend_newhostname不重复_上线.txt | grep -v "^$" >> /etc/storage/wxsend_hostname_上线.txt
+    fi
+    # 下线
+    awk 'NR==FNR{a[$0]++} NR>FNR&&!a[$0]' /tmp/var/wxsend_newhostname.txt /etc/storage/wxsend_hostname_上线.txt > /tmp/var/wxsend_newhostname不重复_下线.txt
+    if [ -s "/tmp/var/wxsend_newhostname不重复_下线.txt" ] ; then
+        content=`cat /tmp/var/wxsend_newhostname不重复_下线.txt | grep -v "^$" | sed "s#[^^]\([0-9]\.\) #\n\1 #g" | awk 'NR==1 {print $1}')"`
+	content2=`cat /tmp/var/wxsend_newhostname不重复_下线.txt | grep -v "^$" | sed "s#[^^]\([0-9]\.\) #\n\1 #g" | awk 'NR==2 {print $1}')"`
+       /etc/storage/wxsend20.sh send_message "【`nvram get computer_name`设备下线提醒】" "${content}" "${content2}" &
+        cat /tmp/var/wxsend_newhostname.txt | grep -v "^$" > /etc/storage/wxsend_hostname_上线.txt
+    fi
+fi
+if [ "$wxsend_notify_4" = "1" ] ; then
+  # 进程守护推送
+  #自定义添加推送内容 下面的内容 内容2 是每段不能超过20个字 所以内容只能20个字 多的填在内容2里 
+  #微信推送命令格式： /etc/storage/wxsend.sh send_message "【标题】" "内容" "内容2" & 
+  #例如检测zerotier进程是否掉了 如下
+  #if [ -z "`pidof zerotier-one`" ]  ; then
+  #killall -9 zerotier-one
+  #  zerotier.sh start
+  # /etc/storage/wxsend.sh send_message "【`nvram get computer_name`进程掉线】" "zerotier掉线，重新启动" &
+  # fi
+
+  echo 测试
+
+
+
+
+fi
+    resub=`expr $resub + 1`
+    [ "$resub" -gt 360 ] && resub=1
+else
+echo "Internet down 互联网断线"
+resub=1
+fi
+#下方为循环检测时间，每隔多久检测一次，单位为秒 如下60 表示每隔一分钟
+sleep 60
+continue
+done
+
+EEE
+	chmod 755 /etc/storage/wxsendfile.sh
+fi
+
+sleep 4
+logger -t "【微信推送】" "运行 /etc/storage/wxsendfile.sh"
+/etc/storage/wxsendfile.sh &
+wxsend_keep
+exit 0
+}
+
+case $1 in
+send_message)
+	send_message "$2" "$3" "$4"
+	;;
+start)
+	wxsend_start
+	;;
+stop)
+	wxsend_close
+	;;
+restart)
+        wxsend_close
+	wxsend_start
+	;;
+*)
+	wxsend_start
+	;;
+esac
+
